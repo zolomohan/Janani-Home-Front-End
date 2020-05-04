ec850907b3ed6c9f6c3b9064ddcfa3ac8666b644
zolomohan <mohanzolomon@gmail.com>
2020-05-04 16:18:48 +0530

    Add jsconfig.json

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './components/App';
-import * as serviceWorker from './helpers/serviceWorker';
+import 'styles/index.css';
+import App from 'components/App';
+import * as serviceWorker from 'helpers/serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
