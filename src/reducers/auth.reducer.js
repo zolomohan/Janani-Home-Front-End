e78e18487a039266ccbcb1384f1a63ae8f88b571
zolomohan <mohanzolomon@gmail.com>
2020-05-05 20:54:12 +0530

    Add Auth Reducer

---
+++ src/reducers/auth.reducer.js
@@ -0,0 +1,56 @@
+import {
+  REGISTER_SUCCESS,
+  LOGIN_SUCCESS,
+  USER_LOADING,
+  USER_LOADED,
+  AUTH_FAIL,
+  LOGOUT_SUCCESS,
+} from 'actions/types';
+
+const initialState = () => ({
+  user: null,
+  isLoading: false,
+  isAuthenticated: false,
+  token: localStorage.getItem('token'),
+});
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case USER_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+      };
+
+    case USER_LOADED:
+      return {
+        ...state,
+        isLoading: false,
+        isAuthenticated: true,
+        user: action.payload,
+      };
+
+    case LOGIN_SUCCESS:
+    case REGISTER_SUCCESS:
+      localStorage.setItem('token', action.payload.token);
+      return {
+        ...state,
+        ...action.payload,
+        isLoading: false,
+        isAuthenticated: true,
+      };
+
+    case AUTH_FAIL:
+    case LOGOUT_SUCCESS:
+      localStorage.removeItem('token');
+      return {
+        user: null,
+        token: null,
+        isLoading: false,
+        isAuthenticated: false,
+      };
+
+    default:
+      return state;
+  }
+};
--- src/reducers/root.reducer.js
+++ src/reducers/root.reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import postReducer from 'reducers/post.reducer';
+import authReducer from 'reducers/auth.reducer';
 
 export default combineReducers({
   postReducer,
+  authReducer,
 });
